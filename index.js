9d7c35c329c0e29283e87ff4cd3d44fac1bcdefe
Nahuel <olmedo926@gmail.com>
2021-06-27 14:42:38 -0300

    getInfo

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function createCard(pokemon) {
     const pokemonEl = document.createElement('div');
     pokemonEl.classList.add('card');
     pokemonEl.classList.add(`${pokemon.types[0].type.name}`)
-    const id = (id) => {
+    const pokeId = (id) => {
         if(id < 10) { return id = "00"+id }
         if(id < 100) { return id = "0"+id }
         if(id > 100) { return id }
@@ -32,29 +32,29 @@ function createCard(pokemon) {
     const pokemonCard = `
         
         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-        <h3 class="card-stats-data">  ${pokemon.name}  </h3>
+        <h3 class="card-stats-data"> ${pokemon.name} </h3>
         <div class="card-stats"> 
             <div class="card-stats-id"> 
-                #<span class="card-stat-idContent">  ${id(pokemon.id)} </span>
+                #<span class="card-stat-idContent"> ${pokeId(pokemon.id)} </span>
             </div>
         </div>
         <p class="card-stats-data"> Type: <span> ${pokemon.types[0].type.name} </span> </p>
-        <p>1</p>
-    `;
+          `;
 
-    pokemonEl.innerHTML = pokemonCard;
-    pokemonContainer.appendChild(pokemonEl);
-}
 
-function pokemonInfo(pokemon) {
-    const pokemonEl = document.createElement('div');
-    pokemonEl.classList.add('pokemon-info');
-
-    const pokemonInfo = `
-        ${pokemon.id}
-
-`;
     pokemonEl.innerHTML = pokemonCard;
     pokemonContainer.appendChild(pokemonEl);
 
 }
+
+async function pokeInfo() {
+    let getPoke =  pokemonContainer.addEventListener('click', e => {
+        let target = e.target;
+        console.log(target)
+        });
+     const url = `https://pokeapi.co/api/v2/pokemon/${this.target}`;
+     const res = await fetch(url);
+     const pokemon = await res.json();
+     console.log(pokemon)
+}
+pokeInfo()
